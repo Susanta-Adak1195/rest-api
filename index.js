dfe57748ebdf2b4161eb99308aa40cb75688bfb0
Susanta-Adak1195 <112160547+Susanta-Adak1195@users.noreply.github.com>
2023-03-31 15:03:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.post("/bfhl", async (req, res) => {
-  const data = req.body;
-  let arr = data.data;
+  const { data } = req.body;
+  let arr = data;
   let even = [];
   let odd = [];
   let alphabet = [];
